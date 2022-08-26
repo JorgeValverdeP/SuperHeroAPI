d822e5272cd17575f6039fa63a49659347b3522f
Jorge Luis Valverde Perez <jorge.valverde@grupobabel.com>
2022-08-26 15:49:16 -0600

    Conexion BD funcional

--- SuperHeroAPI/Controllers/SuperHeroController.cs
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -7,20 +7,72 @@ namespace SuperHeroAPI.Controllers
     [ApiController]
     public class SuperHeroController : ControllerBase
     {
+        private static List<SuperHero> heroes = new List<SuperHero>
+
+            {
+               new SuperHero {
+               Id = 1,
+               Name = "Capitan America",
+               FirstName = "Steve",
+               LastName = "Rogers",
+               Place = "Brooklyn"
+               },
+                  new SuperHero {
+               Id = 2,
+               Name = "Iron Man",
+               FirstName = "Tony",
+               LastName = "Stark",
+               Place = "Long Island"
+               }
+            };
+
+
+
         [HttpGet]
         public async Task<ActionResult<List<SuperHero>>> GET() //El action Resulta permite que al presionar el botón salga la información
         {
-            var heroes = new List<SuperHero>
-            {
-                new SuperHero {
-                    Id=1,
-                    Name="Capitan America",
-                    FirstName="Steve",
-                    LastName="Rogers",
-                    Place="Brooklyn"
-                 }
-            };
             return Ok(heroes);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SuperHero>> GET(int id) //El action Resulta permite que al presionar el botón salga la información
+        {
+            var hero = heroes.Find(h => h.Id == id);
+            if (hero == null)
+                return BadRequest("Heroe no existe");
+            return Ok(hero);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero) //El action Resulta permite que al presionar el botón salga la información
+        {
+            heroes.Add(hero);
+            return Ok(heroes);
+        }
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero (SuperHero request) //El action Resulta permite que al presionar el botón salga la información
+        {
+            var hero = heroes.Find(h => h.Id == request.Id);
+            if (hero == null)
+                return BadRequest("Heroe no existe");
+            
+            hero.Name = request.Name;
+            hero.FirstName = request.FirstName;
+            hero.LastName = request.LastName;
+            hero.Place = request.Place;
+            return Ok(heroes);
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> DeleteHero(int id) 
+        {
+            var hero = heroes.Find(h => h.Id == id);
+            if (hero == null)
+                return BadRequest("Heroe no existe");
+                heroes.Remove(hero);
+
+            return Ok(hero);
         }
     }
 }
