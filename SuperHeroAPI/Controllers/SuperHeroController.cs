93e3451d7c564969370a148ec137f55343d46b39
Jorge Luis Valverde Perez <jorge.valverde@grupobabel.com>
2022-08-25 22:23:36 -0600

    Add project files.

---
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GET() //El action Resulta permite que al presionar el botón salga la información
+        {
+            var heroes = new List<SuperHero>
+            {
+                new SuperHero {
+                    Id=1,
+                    Name="Capitan America",
+                    FirstName="Steve",
+                    LastName="Rogers",
+                    Place="Brooklyn"
+                 }
+            };
+            return Ok(heroes);
+        }
+    }
+}
---
+++ SuperHeroAPI/SuperHero.cs
@@ -0,0 +1,13 @@
+﻿namespace SuperHeroAPI
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }     
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public string Place { get; set; }
+
+         
+    }
+}
